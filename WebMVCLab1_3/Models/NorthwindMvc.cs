21b1d24e839bc420df3d0503298a88717fa5b1e3
Bruce <gkm85663@gmail.com>
2019-06-15 13:56:51 +0800

    add NorthwindMvc DB and import Order_Details and Products tables

---
+++ WebMVCLab1_3/Models/NorthwindMvc.cs
@@ -0,0 +1,34 @@
+namespace WebMVCLab1_3.Models
+{
+    using System;
+    using System.Data.Entity;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Linq;
+
+    public partial class NorthwindMvc : DbContext
+    {
+        public NorthwindMvc()
+            : base("name=NorthwindMvc")
+        {
+        }
+
+        public virtual DbSet<Order_Detail> Order_Details { get; set; }
+        public virtual DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Order_Detail>()
+                .Property(e => e.UnitPrice)
+                .HasPrecision(19, 4);
+
+            modelBuilder.Entity<Product>()
+                .Property(e => e.UnitPrice)
+                .HasPrecision(19, 4);
+
+            modelBuilder.Entity<Product>()
+                .HasMany(e => e.Order_Details)
+                .WithRequired(e => e.Product)
+                .WillCascadeOnDelete(false);
+        }
+    }
+}
