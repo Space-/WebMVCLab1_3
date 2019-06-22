dc4b58a7c61e1fec40c3160b338a5c5ffa79b1f6
Bruce <gkm85663@gmail.com>
2019-06-22 16:28:34 +0800

    add Help page package

---
+++ WebMVCLab1_3/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace WebMVCLab1_3.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ WebMVCLab1_3/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace WebMVCLab1_3.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
